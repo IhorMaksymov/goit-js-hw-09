bdd3511d721658b5ab401aeac3fc9773256727d7
IhorMaksymov <bonefadze0ne@gmail.com>
2022-07-31 17:28:03 +0300

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,6 +4,8 @@ const refs = {
     btnStop: document.querySelector('[data-stop]'),
 }
 
+let timerEve = null;
+
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
